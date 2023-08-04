c045084345ba847459dd31c13d7bff0734aa1305
AayushMohan <aayushmohan1702@gmail.com>
2023-08-04 13:20:39 +0530

    Added React TypeWriter Effect

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+"use client";
 import BackgroundCircles from "./BackgroundCircles";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 
 type Props = {};
 
 const Hero = (props: Props) => {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hey, The Name's Aayush Mohan",
+      "Guy-who-loves-coffee.tsx",
+      "<ButLovesToCodeMore />",
+    ],
+    loop: true,
+    delaySpeed: 200,
+  });
   return (
     <div>
       <BackgroundCircles />
