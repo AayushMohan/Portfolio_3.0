371d14fa5950640c109ff750e9a8fbb9bf4604cd
AayushMohan <aayushmohan1702@gmail.com>
2023-08-04 23:50:36 +0530

    Updated OpenSeas

--- components/Projects/OpenSea.tsx
+++ components/Projects/OpenSea.tsx
@@ -4,19 +4,27 @@ type Props = {};
 
 const OpenSea = (props: Props) => {
   return (
-    <div className="relative w-full h-screen flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
-      <motion.img
-        initial={{ y: -300, opacity: 0 }}
-        transition={{ duration: 1.2 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        viewport={{ once: true }}
-        src="https://cdn.sanity.io/images/p31j23cd/production/73257f0b516c4df9a372565178b86b97b9838771-3104x1788.png"
-        width="350"
-        height="450"
-        className="md:mr-0 rounded-lg md:w-[500px] md:h-[380px] object-cover"
-      />
+    <div>
+      <div className="relative w-full h-screen flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-around p-4 md:py-48 h-screen">
+          <h3 className="uppercase tracking-[20px] text-gray-500 text-xl md:text-4xl md:my-8 my-2">
+            Projects
+          </h3>
 
-      <div className="space-y-10"></div>
+          <motion.img
+            initial={{ y: -300, opacity: 0 }}
+            transition={{ duration: 1.2 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            src="https://cdn.sanity.io/images/p31j23cd/production/73257f0b516c4df9a372565178b86b97b9838771-3104x1788.png"
+            width="350"
+            height="450"
+            className="md:mr-0 rounded-lg md:w-[500px] md:h-[380px] object-cover"
+          />
+
+          <div className="space-y-10"></div>
+        </div>
+      </div>
     </div>
   );
 };
