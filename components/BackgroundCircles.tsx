3379853595c50ae3ae02835e1135125c51a94622
AayushMohan <aayushmohan1702@gmail.com>
2023-08-04 13:12:47 +0530

    Added Motion

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { motion } from "framer-motion";
 
@@ -9,7 +10,7 @@ const BackgroundCircles = ({}: Props) => {
       initial={{ opacity: 0 }}
       animate={{
         scale: [1, 2, 2, 3, 1],
-        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 0.1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
         borderRadius: ["20%", "20%", "50%", "80%", "20%"],
       }}
       transition={{ duration: 2.5 }}
