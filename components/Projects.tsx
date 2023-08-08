fa5848dff2e6e8a4659cf700f02f8b4013a8e1e9
AayushMohan <aayushmohan1702@gmail.com>
2023-08-08 23:15:28 +0530

    Added Project Description

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,6 +2,7 @@
 import { Project } from "@/projects";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 type Props = {
   projects: Project[];
@@ -17,7 +18,7 @@ const Projects = ({ projects }: Props) => {
       transition={{ duration: 1.5 }}
       className="h-screen relative flex flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
     >
-      <div className="relative w-full h-screen flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+      <div className="relative w-full h-screen flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 scrollbar-thin">
         {projects?.map((projects, i) => (
           <div
             key={i}
@@ -41,13 +42,43 @@ const Projects = ({ projects }: Props) => {
             <div className="space-y-10 px-0 md:px-10 max-w-6xl md:pb-4">
               <h4 className="text-xl md:text-2xl font-semibold text-center">
                 <span className="underline decoration-[#F7AB0A]/50">
-                  Case Study {i + 1} of {totalProjects}
+                  Case Study {i + 1} of {totalProjects} :{" "}
                 </span>
+                {projects.name}
               </h4>
+
+              <div></div>
+              <p className="text-md py-4 px-2 leading-relaxed text-center md:mx-16">
+                {projects.description}
+              </p>
+
+              <div className="flex items-center justify-evenly">
+                {projects.liveUrl && (
+                  <a
+                    href={projects.liveUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                  >
+                    Live Demo
+                  </a>
+                )}
+                {projects.codebaseUrl && (
+                  <a
+                    href={projects.codebaseUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600"
+                  >
+                    Codebase
+                  </a>
+                )}
+              </div>
             </div>
           </div>
         ))}
       </div>
+      <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12" />
     </motion.div>
   );
 };
--- projects.ts
+++ projects.ts
@@ -11,17 +11,19 @@ export interface Project {
 
 const projects: Project[] = [
   {
-    name: 'Project 1',
-    description: 'Description for Project 1',
-    liveUrl: 'https://www.example.com/project1',
+    name: 'The News App',
+
+    description: "News App is a mobile-optimized web application that allows users to browse and filter news articles from multiple sources.It is built using Next.js, TypeScript, TailwindCSS, StepZen API, and GraphQL.This project showcases my proficiency in modern web development technologies and my ability to build complex applications with a clean codebase.",
+
+    liveUrl: 'https://news-app-sigma-khaki.vercel.app/',
     imageUrl: "https://cdn.sanity.io/images/p31j23cd/production/8ba754197bf8973a5bebd4220605e4f5b6053830-3104x1788.png?w=2000&fit=max&auto=format&dpr=2",
-    codebaseUrl: 'https://github.com/AayushMohan/NewsApp',
+    codebaseUrl: "https://github.com/AayushMohan/news-app",
   },
   {
     name: 'Project 2',
     description: 'Description for Project 2',
     liveUrl: 'https://www.example.com/project2',
-    imageUrl: "https://cdn.sanity.io/images/p31j23cd/production/8ba754197bf8973a5bebd4220605e4f5b6053830-3104x1788.png?w=2000&fit=max&auto=format&dpr=2",
+    imageUrl: "https://cdn.sanity.io/images/p31j23cd/production/73257f0b516c4df9a372565178b86b97b9838771-3104x1788.png",
     codebaseUrl: 'https://github.com/yourusername/project2',
   }, {
     name: 'Project 3',
