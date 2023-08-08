6343877040807681b2f41f93c621f32bff2cdf1f
AayushMohan <aayushmohan1702@gmail.com>
2023-08-08 23:49:08 +0530

    Added Coming Soon

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -3,7 +3,14 @@ import React from "react";
 type Props = {};
 
 const Blogs = (props: Props) => {
-  return <div className="h-screen snap-start">My Blogs</div>;
+  return (
+    <div className="h-screen snap-start">
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="text-4xl font-bold">Blogs</h1>
+        <p className="text-xl">Coming Soon</p>
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
