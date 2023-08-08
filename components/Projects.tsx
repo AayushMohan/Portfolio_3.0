0c925069ca3ab4154ea29e41335197d962253ffa
AayushMohan <aayushmohan1702@gmail.com>
2023-08-08 21:53:11 +0530

    Added Motion Div

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,7 +7,16 @@ type Props = {
 };
 
 const Projects = ({ projects }: Props) => {
-  return <motion.div></motion.div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen relative flex flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+    >
+      <div></div>
+    </motion.div>
+  );
 };
 
 export default Projects;
