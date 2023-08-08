d1998cf5a6636f7385f51fcde0c7bbcf843d3113
AayushMohan <aayushmohan1702@gmail.com>
2023-08-09 01:00:07 +0530

    Added Header Icons

--- socials.ts
+++ socials.ts
@@ -1,4 +1,5 @@
 export interface Social {
+ id: number;
  name: string;
  url: string;
  icon: string;
@@ -6,21 +7,25 @@ export interface Social {
 
 const socials: Social[] = [
  {
+  id: 1,
   name: 'Github',
   url: 'https://github.com/AayushMohan',
   icon: 'github',
  },
  {
+  id: 2,
   name: 'LinkedIn',
   url: 'https://www.linkedin.com/in/AayushMohan/',
   icon: 'linkedin',
  },
  {
+  id: 3,
   name: 'Twitter',
   url: 'https://twitter.com/AayushMohan',
   icon: 'twitter',
  },
  {
+  id: 4,
   name: 'Instagram',
   url: 'https://www.instagram.com/thisisaayushmohan/',
   icon: 'instagram',
