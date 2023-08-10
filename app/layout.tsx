9bf2bead57f1a49dbc6a4bd650833853b820bda7
AayushMohan <aayushmohan1702@gmail.com>
2023-08-10 16:19:24 +0530

    Updated Folder Structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,6 @@
-import Header from "@/components/Header";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Skills from "@/components/Skills";
-import projects from "../projects";
-import Projects from "@/components/Projects";
-import Blogs from "@/components/Blogs";
-import ContactMe from "@/components/ContactMe";
-import socials from "@/socials";
 
 const open_sans = Open_Sans({ subsets: ["latin"] });
 
@@ -25,36 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={open_sans.className}>
-        <section className="bg-black text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
-          <Header socials={socials} />
-
-          <section id="hero" className="snap-start">
-            <Hero />
-          </section>
-
-          <section id="about" className="snap-center">
-            <About />
-          </section>
-
-          <section id="skills" className="snap-end">
-            <Skills />
-          </section>
-
-          <section id="projects" className="snap-start">
-            <Projects projects={projects} />
-          </section>
-
-          {/* <section id="Blogs" className="snap-center">
-            <Blogs />
-          </section> */}
-
-          <section id="Contact" className="snap-start">
-            <ContactMe />
-          </section>
-          {children}
-        </section>
-      </body>
+      <body className={open_sans.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,41 @@
-export default function Home() {
-  return <div className=""></div>;
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Skills from "@/components/Skills";
+import projects from "../projects";
+import Projects from "@/components/Projects";
+// import Blogs from "@/components/Blogs";
+import ContactMe from "@/components/ContactMe";
+import socials from "@/socials";
+
+export default function Home({ post }: any) {
+  return (
+    <main className="bg-black text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
+      <Header socials={socials} />
+
+      <section id="hero" className="snap-start">
+        <Hero />
+      </section>
+
+      <section id="about" className="snap-center">
+        <About />
+      </section>
+
+      <section id="skills" className="snap-end">
+        <Skills />
+      </section>
+
+      <section id="projects" className="snap-start">
+        <Projects projects={projects} />
+      </section>
+
+      {/* <section id="Blogs" className="snap-center">
+        <Blogs />
+      </section> */}
+
+      <section id="Contact" className="snap-start">
+        <ContactMe />
+      </section>
+    </main>
+  );
 }
