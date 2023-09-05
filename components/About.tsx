9e7d8e1b0a46dd698ce111b1b9a265aa700e6d52
AayushMohan <aayushmohan1702@gmail.com>
2023-09-05 23:47:37 +0530

    Updated About

--- components/About.tsx
+++ components/About.tsx
@@ -11,7 +11,7 @@ const About = (props: Props) => {
       transition={{ duration: 1 }}
       className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 py-10 justify-evenly mx-auto items-center mt-40"
     >
-      <h3 className="text-center md:absolute top-24 md:-ml-20 uppercase tracking-[12px] md:tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="text-center md:absolute top-24 md:-ml-16 uppercase tracking-[12px] md:tracking-[20px] text-gray-500 text-4xl">
         About
       </h3>
 
@@ -21,7 +21,7 @@ const About = (props: Props) => {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src="https://cdn.sanity.io/images/p31j23cd/production/214b077f7a78bf10d1dd8c47afd9748583c60d51-3008x4624.jpg?w=2000&fit=max&auto=format&dpr=2"
-        className="md:mb-0 flex-shrink-0 w-40 h-40 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[500px]"
+        className="md:mb-0 flex-shrink-0 w-40 h-40 rounded-full object-cover md:rounded-lg md:w-64 xl:w-[350px] xl:h-[500px]"
         alt="User Profile Pic"
       />
 
