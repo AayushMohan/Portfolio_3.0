d2c7c75e45f521fc7443713395966da43d53d701
AayushMohan <aayushmohan1702@gmail.com>
2023-09-11 23:09:01 +0530

    Added Flex

--- app/activity/index.tsx
+++ app/activity/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type Props = {};
 
 const index = (props: Props) => {
-  return <div className="h-screen pt-10 m-10"></div>;
+  return <div className="h-screen pt-10 m-10 flex flex-col"></div>;
 };
 
 export default index;
