4e06ecebeb70d8482fa25ffaee3ed4fef85e7854
AayushMohan <aayushmohan1702@gmail.com>
2023-09-11 23:13:02 +0530

    Added Framer

--- app/activity/index.tsx
+++ app/activity/index.tsx
@@ -6,7 +6,11 @@ type Props = {};
 const index = (props: Props) => {
   return (
     <div className="h-screen pt-10 m-10 flex flex-col space-y-3">
-      <motion.div></motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+      ></motion.div>
     </div>
   );
 };
