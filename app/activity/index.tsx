8520303ef7b10f7b57a946db6a462274e377d3a5
AayushMohan <aayushmohan1702@gmail.com>
2023-09-11 23:12:31 +0530

    Added Framer Motion

--- app/activity/index.tsx
+++ app/activity/index.tsx
@@ -1,9 +1,14 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 type Props = {};
 
 const index = (props: Props) => {
-  return <div className="h-screen pt-10 m-10 flex flex-col space-y-3"></div>;
+  return (
+    <div className="h-screen pt-10 m-10 flex flex-col space-y-3">
+      <motion.div></motion.div>
+    </div>
+  );
 };
 
 export default index;
