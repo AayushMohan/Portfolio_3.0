846d2286bfcdc0c88f296b4295f3d69a3c57e1d0
AayushMohan <aayushmohan1702@gmail.com>
2023-09-16 23:41:53 +0530

    Updated Duration

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -51,7 +51,7 @@ const Skill = (props: Props) => {
         alt="JavaScript"
         height="50"
         width="50"
-        className="translation ease-in-out duration-500 transform hover:scale-110"
+        className="translation ease-in-out duration-300 transform hover:scale-110"
       />
       <Image
         src="https://img.icons8.com/plasticine/100/null/react.png"
