ade15f93e44edd91e6fd61acbb4128311146be70
AayushMohan <aayushmohan1702@gmail.com>
2023-09-16 23:41:27 +0530

    Updated Duration

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -35,7 +35,7 @@ const Skill = (props: Props) => {
         alt="Bootstrap"
         height="50"
         width="50"
-        className="translation ease-in-out duration-500 transform hover:scale-110"
+        className="translation ease-in-out duration-300 transform hover:scale-110"
       />
 
       <Image
