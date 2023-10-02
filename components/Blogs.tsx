8a9e2e08f7b3efa10025c86d3dbcb0f2a115243b
AayushMohan <aayushmohan1702@gmail.com>
2023-10-02 23:30:27 +0530

    Updated Arrow

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -39,7 +39,7 @@ const Blogs = (props: Props) => {
         </div>
 
         {/* <div className="flex items-center"> */}
-        <ArrowLongRightIcon className="h-10 text-[#F7AB0A]/80" />
+        {/* <ArrowLongRightIcon className="h-10 text-[#F7AB0A]/80" /> */}
         {/* </div> */}
       </div>
     </div>
