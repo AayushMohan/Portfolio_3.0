02f392580814ae8542bd4f58b039a539ede0bab9
AayushMohan <aayushmohan1702@gmail.com>
2024-05-26 00:13:39 +0530

    chore: Update Header component to enable email functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -50,10 +50,10 @@ function Header({ socials }: Props) {
           network="email"
           fgColor="gray"
           bgColor="transparent"
-          // href="mailto:am@aayushmohan.dev"
-          // onClick={() => {
-          //   window.open("mailto:am@aayushmohan.dev");
-          // }}
+          href="mailto:am@aayushmohan.dev"
+          onClick={() => {
+            window.open("mailto:am@aayushmohan.dev");
+          }}
         />
         <Link href="#contact">
           <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
