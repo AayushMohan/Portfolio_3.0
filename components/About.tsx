f1149952d371ff8dbfcaa5ce667adbe2ee4a812a
AayushMohan <aayushmohan1702@gmail.com>
2024-05-25 22:19:01 +0530

    chore: Update styling and dependencies for Blogs and Projects components

--- components/About.tsx
+++ components/About.tsx
@@ -11,7 +11,7 @@ const About = (props: Props) => {
       transition={{ duration: 1 }}
       className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 py-10 justify-evenly mx-auto items-center mt-40"
     >
-      <h3 className="text-center md:absolute top-24 md:-ml-36 uppercase tracking-[12px] md:tracking-[20px] text-gray-500 text-4xl">
+      <h3 className="text-center md:absolute top-24 mt-10 mb-3 md:-ml-36 uppercase tracking-[12px] md:tracking-[20px] text-gray-500 text-3xl md:text-4xl">
         About
       </h3>
 
@@ -21,23 +21,25 @@ const About = (props: Props) => {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src="https://cdn.sanity.io/images/p31j23cd/production/214b077f7a78bf10d1dd8c47afd9748583c60d51-3008x4624.jpg?w=2000&fit=max&auto=format&dpr=2"
-        className="md:mb-0 flex-shrink-0 w-40 h-40 rounded-full object-cover md:rounded-lg md:w-64 xl:w-[350px] xl:h-[500px]"
+        className="md:mb-0 flex-shrink-0 w-36 h-36 rounded-full object-cover md:rounded-lg md:w-64 xl:w-[350px] xl:h-[500px]"
         alt="User Profile Pic"
       />
 
       <div className="space-y-10 px-0 md:px-16 md:text-left text-center">
-        <h4 className="font-bold text-4xl m my-10 text-gray-200">
+        <h4 className="font-bold text-3xl md:text-4xl my-4 md:my-10 text-gray-200">
           Here is a{" "}
           <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
           background
         </h4>
-        <p className="text-base leading-relaxed md:leading-loose text-gray-300">
+        <p className="text-sm md:text-base leading-loose md:leading-loose text-gray-300">
           Web developer with a passion for AI and ML, pursuing B.Tech in AI/ML.
-          Strong foundation in web development with expertise in JavaScript,
-          HTML, CSS, React, Node.js. Actively expanding knowledge in AI and ML.
-          Thrives in fast-paced environments, enjoys challenging projects, and
-          believes in continuous learning. Let&apos;s connect to shape the
-          future of technology with AI and ML!
+          I offer professional web development services with expertise in
+          JavaScript, TypeScript, React, and Node.js. Alongside my development
+          work, I am actively exploring deep learning and its applications. I
+          thrive in fast-paced environments, enjoy tackling challenging
+          projects, and believe in continuous learning. Let&apos;s connect to
+          create innovative web solutions and explore the future of technology
+          together!
         </p>
       </div>
     </motion.div>
--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
@@ -27,11 +28,17 @@ const ContactMe = (props: Props) => {
       <div className="flex flex-col space-y-10 text-gray-300">
         <h4 className="md:text-4xl text-lg font-semibold text-center">
           I have got just what you need.{" "}
-          <span className="decoration-[#F7AB0A] underline animate-pulse">
-            Lets Talk.
-          </span>
+          <Link href="https://calendly.com/aayushmohan/wr">
+            <span className="relative group">
+              <span className="decoration-[#F7AB0A] underline animate-pulse">
+                Let&apos;s Talk.
+              </span>
+              <span className="absolute top-10 left-20 transform translate-x-1 mt-2 w-max p-2 text-sm text-white bg-gray-800/40 rounded opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                Click to schedule a meeting on Calendly
+              </span>
+            </span>
+          </Link>
         </h4>
-
         <div className="space-y-10">
           <div className="flex items-center md:space-x-5 space-x-2 justify-center ">
             <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
